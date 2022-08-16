d0f1ba716a03e56441fbfeda504ecb30f279a120
josemaria1988 <idontknow1988@gmail.com>
2022-08-16 20:26:07 -0300

    componente Navbar creado

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import './NavBar.css'
+import logo from '../assets/img/logo.png'
+
+const Navbar = () => {
+    return (
+    <header className="navHeader">
+        <nav className="navBar">
+            <a href="#"><img src={logo} alt="logo" className="logo"></img></a>
+            <ul className="ulNav">
+                <li className="nav-menu-item"><a href="#" className="nav-link">Inicio</a></li>
+                <li className="nav-menu-item"><a href="#" className="nav-link">Productos</a></li>
+                <li className="nav-menu-item"><a href="#" className="nav-link">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+}
+
+export default Navbar
