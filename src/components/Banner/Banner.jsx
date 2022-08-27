f2df23a1204d78c6e084e4320d25fb825e66b603
josmasos <josemaria@rontil.com>
2022-08-27 12:17:40 -0300

    desafío clase 5 bastante bastante, una cagada

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Banner.scss'
 import portada2 from '../../assets/img/portada2.jpg'
+import MoonLoader from 'react-spinners/MoonLoader'
 
-export default function Banner() {
+
+
+const Banner = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    })
+
     return (
+        <>
+        {loading ? <div className="spinner"><MoonLoader /> </div>
+        :
         <div className="banner-container">
-            <img className="bannerImage" src={portada2} alt="banner"/>
+            <img calssName="bannerImage" src={portada2} alt="banner" />
         </div>
+    }
+        </>
     )
 }
+
+export default Banner
