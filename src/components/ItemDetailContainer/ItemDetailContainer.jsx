f8f575f206df12b5a65190bebd4e58b6dfdad932
josmasos <josemaria@rontil.com>
2022-08-28 15:24:24 -0300

    esperando la siguiente clase porque no se todavia como se hace

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import './ItemDetailContainer.scss'
+import { pedirDatos } from "../../helpers/pedirDatos"
+import ItemDetail from "../ItemList/ItemList"
+import MoonLoader from "react-spinners/MoonLoader";
 
 const ItemDetailContainer = () => {
 
-    return (
-        <div>
+  const [productos, setProductos] = useState([])
+  const [loading, setLoading] = useState(true)
 
-        </div>
-    )
+    useEffect(() => {
+        pedirDatos()
+            .then( (res) => {
+                setProductos(res)
+                setLoading(false)
+            })
+            .catch( (error) => {
+              setLoading(false)
+                console.log(error)
+            })
+            .finally(() => {
+                // console.log("Fin del proceso")
+            })
+    }, [])
+  
+  return (
+  <>
+    {loading ? <div className="spinner" ><MoonLoader/> </div>  
+     : 
+    <div className="divCards">
+      <h4 className="cardsTittle">Detalle de Producto</h4>
+
+      <ItemDetail productos={productos}/>
+
+    </div>
+  }
+    </>
+  );
 }
 
 export default ItemDetailContainer
