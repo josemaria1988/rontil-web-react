0cca6dea536c1516783bb2d82497ab427638df85
josemaria1988 <idontknow1988@gmail.com>
2022-08-30 20:19:41 -0300

    guardando para testear fuerte

--- src/components/SingleItem/SingleItem.jsx
+++ src/components/SingleItem/SingleItem.jsx
@@ -1,14 +1,47 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './SingleItem.scss'
+import ItemCount from '../ItemCount/ItemCount.jsx'
 
 
 const Item = ({ producto }) => {
 
+    const [counter, setCounter] = useState(1);
+
+    const onAdd = () => {
+        if(counter < producto.stock) setCounter(counter+1);
+    }
+
+    const onDecrement = () => {
+        if(counter > producto.cantidad) setCounter(counter-1);
+    }
+
    return (
-    <div>
-        <h2>{producto.nombre}</h2>
-        <h3>{producto.precio}</h3>
+    <div className="item-detail-father">
+        <div className="item-detail-image">
+            <img src={producto.img} className="big-img" alt=""/>
+        </div>
+        <div className="box-detail">
+            <h2>{producto.nombre}</h2>
+            <h3>$ {producto.precio}</h3>
+        </div>
+        <div className="variantes-color">
+            {producto.color.map(color => (
+                <button style={{background: color}}></button>
+            ))}
+        </div>
+        <p>{producto.desc}</p>
+        <p>ñ{producto.stock}</p>
+        <div className="thumbnail">
+            {
+                producto.img.map( img => (
+                    <img src={img} alt=""/>
+                ))
+            }
+        </div>
+        <ItemCount precio={producto.precio} counter={counter} onAdd={onAdd} onDecrement={onDecrement}/>
+        <button className="btn-comprar">Añadir al Carrito</button>
+
     </div>
    )
 }
--- src/data/stockProductos.js
+++ src/data/stockProductos.js
@@ -6,7 +6,10 @@ let stockProductos = [{
   desc: "Contenedor SULO de 120 Lts", 
   precio: 3100,  
   stock: 10,
-  img: "https://http2.mlstatic.com/D_NQ_NP_798192-MLU44296538377_122020-O.webp"
+  color:  ["black", "blue", "red", "yellow", "white"],
+  img: ["https://http2.mlstatic.com/D_NQ_NP_798192-MLU44296538377_122020-O.webp",
+        'https://http2.mlstatic.com/D_NQ_NP_883628-MLU50452881879_062022-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_883628-MLU50452881879_062022-O.webp']
 },
 {
   id: 2, 
@@ -16,7 +19,10 @@ let stockProductos = [{
   desc: "Contenedor SULO de 240 Lts", 
   precio: 4200, 
   stock: 10,
-  img: 'https://http2.mlstatic.com/D_NQ_NP_693270-MLU49787249841_042022-O.webp'
+  color:  ["black", "blue", "red", "yellow", "white"],
+  img: ['https://http2.mlstatic.com/D_NQ_NP_693270-MLU49787249841_042022-O.webp', 
+        'https://http2.mlstatic.com/D_NQ_NP_883628-MLU50452881879_062022-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp']
 },
 {
   id: 3, 
@@ -26,7 +32,10 @@ let stockProductos = [{
   desc: "Contenedor SULO de 360 Lts", 
   precio: 6300,  
   stock: 10,
-  img: 'https://http2.mlstatic.com/D_NQ_NP_903332-MLU31241501031_062019-O.webp'
+  color:  ["black", "blue", "red", "yellow", "white"],
+  img: ['https://http2.mlstatic.com/D_NQ_NP_903332-MLU31241501031_062019-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp']
 },
 {
   id: 4, 
@@ -36,7 +45,10 @@ let stockProductos = [{
   desc: "Contenedor SULO Citybac 800 Lts", 
   precio: 18800, 
   stock: 10,
-  img: 'https://http2.mlstatic.com/D_NQ_NP_752631-MLU31241259807_062019-O.webp'
+  color:  ["black", "blue", "red", "yellow", "white"],
+  img: ['https://http2.mlstatic.com/D_NQ_NP_752631-MLU31241259807_062019-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp']
 },
 {
   id: 5,
@@ -46,7 +58,10 @@ let stockProductos = [{
   desc: 'Contenedor SULO Citybac 1100 Lts',
   precio: 20100,
   stock: 10,
-  img: 'https://http2.mlstatic.com/D_NQ_NP_810951-MLU32027207841_082019-O.webp'
+  color:  ["black", "blue", "red", "yellow", "white"],
+  img: ['https://http2.mlstatic.com/D_NQ_NP_810951-MLU32027207841_082019-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp',
+        'https://http2.mlstatic.com/D_NQ_NP_749094-MLU49546975295_042022-O.webp']
 }
 ];
 
