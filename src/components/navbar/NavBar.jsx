11153c64b71c09cbafc66342f7dffcd6869a457e
josemaria1988 <idontknow1988@gmail.com>
2022-09-04 23:16:13 -0300

    css sin solucion

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,18 +4,28 @@ import './NavBar.scss'
 import logo from '../../assets/img/logo.png'
 import CartWidget from '../CartWidget/CartWidget.jsx'
 import {Link} from 'react-router-dom';
+import {useState} from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
     <header className="navHeader">
         <nav className="navBar">
-            <Link to='/'><img src={logo} alt="logo" className="logo"></img></Link>
-            <ul className="ulNav">
+            
+                <Link to='/'><img src={logo} alt="logo" className="logo"></img></Link>
+            
+            <ul className={`ulNav ${isOpen && "open"}`}>
                 <li className="nav-menu-item"><Link to="/" className="nav-link">Inicio</Link></li>
                 <li className="nav-menu-item"><Link to='/productos' className="nav-link">Productos</Link></li>
                 <li className="nav-menu-item"><Link to='/nosotros' className="nav-link">Nosotros</Link></li>
                 <li className="nav-menu-item"><Link to='/contacto' className="nav-link">Contacto</Link></li>
             </ul>
+            <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => setIsOpen(isOpen)}>
+                <div className="nav-btn"><MenuIcon/></div>
+            </div>
             {/* Agregamos el CartWidget al nav */}
             <CartWidget/>
         </nav>
