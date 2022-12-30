bb41b8c0687ceb8107cec4b477a029676887a4ac
josmasos <josemaria@rontil.com>
2022-12-30 12:01:31 -0300

    actualizando todas las ramas

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,52 +1,11 @@
 import React from 'react';
 import './Inicio.scss';
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@mui/material';
-import secadoratromink from '../../assets/img/secadoratromink.jpg'
-import portada2 from '../../assets/img/portada2.jpg'
-import jorgensen from '../../assets/img/jorgensen.jpg'
-
-
-const Item = (props) => {
-    return (
-            <Paper className="paper-carousel">
-                <h2 className="titulo-carousel">{props.item.name}</h2>
-                <img src={props.item.img} alt={props.item.imge}/>
-                <Button className="CheckButton">
-                    Ver más!
-                </Button>
-            </Paper>
-    )
-}
+import Banner from '../Banner/Banner.jsx'
 
 const Inicio = (props) => {
 
-    const items = [
-        {
-            name: "SULO",
-            description: "Random description for this item",
-            img: portada2
-        },
-        {
-            name: "TROMINK",
-            description: "Even more random description for this",
-            img: secadoratromink
-        },
-        {
-            name: "JORGENSEN",
-            description: "A ver si funciona",
-            img: jorgensen
-        }
-    ]
-
     return (
-        <div className="caja-inicio">
-        <Carousel>
-            {
-                items.map((item, i) => <Item key={i} item={item} />)
-            }
-        </Carousel>
-        </div>
+        <Banner/>
     )
 }
 
